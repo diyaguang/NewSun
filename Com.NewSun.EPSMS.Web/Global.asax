4aecd13446c41ee001e69078691fc55cd4d1b4c5
diyaguang@163.com <diyaguang@163.com>
2015-05-20 22:27:06 +0800

    新增加项目

---
+++ Com.NewSun.EPSMS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Com.NewSun.EPSMS.Web.MvcApplication" Language="C#" %>
---
+++ NewSun.EPSMS.Model/Account/AccountEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Com.NewSun.DataAccess;
+
+namespace Com.NewSun.EPSMS.Model
+{
+     [Serializable]
+    public class Account : BaseModel
+    {
+        
+    }
+}
